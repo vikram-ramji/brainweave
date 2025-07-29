0c06f91cf23cc0ea8865969c2bad7e5cd4152efe
Vikram Ramji Iyer <vikramramji24@gmail.com>
2025-07-29 08:21:28 +0000

    fix: fixed create note disabled button bug

--- src/components/app/AppHeader.tsx
+++ src/components/app/AppHeader.tsx
@@ -37,7 +37,7 @@ export function AppHeader({
             variant={"outline"}
             type="submit"
             form={isCreateMode ? "note-create-form" : "note-form"}
-            disabled={!hasChanged|| isSubmitting}
+            disabled={!isCreateMode && !hasChanged || isSubmitting}
             className="px-4 py-2 bg-background rounded transition-colors cursor-pointer"
           >
             {isSubmitting
