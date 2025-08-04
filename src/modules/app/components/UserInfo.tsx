a56c0486dc48f1b0601fc62e07badd80fc7c401a
vikram <vikramramji24@gmail.com>
2025-08-04 22:37:35 +0530

    Update src/modules/app/components/UserInfo.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/modules/app/components/UserInfo.tsx
+++ src/modules/app/components/UserInfo.tsx
@@ -4,7 +4,7 @@ import { User } from "better-auth";
 
 export default function UserInfo({ user }: { user: User }) {
   return (
-    <div className={`flex items-center`}>
+    <div className="flex items-center gap-3">
       {user.image ? (
         <Avatar>
           <AvatarImage src={user.image} alt={user.name || "User Avatar"} />
@@ -16,12 +16,11 @@ export default function UserInfo({ user }: { user: User }) {
           className="size-9 mr-3"
         />
       )}
-      <div className="flex flex-col gap-0.2 text-left overflow-hidden flex-1 min-w-0">
+      <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
         <span className="text-sm truncate w-full">{user.name}</span>
         <span className="text-xs text-muted-foreground truncate w-full">
           {user.email}
         </span>
       </div>
     </div>
-  );
 }
