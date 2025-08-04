e0ee286aa3f584c28639e7ff3eaee7498b9d33dc
Vikram Ramji Iyer <vikramramji24@gmail.com>
2025-08-04 16:49:13 +0000

    feat: add sidebar UI components and mobile hook

---
+++ src/components/GeneratedAvatar.tsx
@@ -0,0 +1,35 @@
+import { createAvatar } from "@dicebear/core"
+import { botttsNeutral, loreleiNeutral } from "@dicebear/collection";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { cn } from "@/lib/utils";
+import { AvatarImage } from "@radix-ui/react-avatar";
+
+interface GeneratedAvatarProps {
+    seed: string;
+    className?: string;
+    variant: "botttsNeutral" | "loreleiNeutral"
+}
+
+export default function GeneratedAvatar({
+    seed,
+    className,
+    variant
+}: GeneratedAvatarProps) {
+    let avatar;
+  if (variant === "botttsNeutral") {
+    avatar = createAvatar(botttsNeutral, {
+        seed
+    })
+  } else {
+    avatar = createAvatar(loreleiNeutral, {
+        seed
+    })
+  }
+
+  return (
+    <Avatar className={cn(className)}>
+        <AvatarImage src={avatar.toDataUri()} />
+        <AvatarFallback>{seed.charAt(0).toUpperCase()}</AvatarFallback>
+    </Avatar>
+  )
+}
